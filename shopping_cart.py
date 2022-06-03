774bc101d65934174ab40c7af843a33cde33506a
ealmiras <ealmirasozlu@gmail.com>
2022-06-03 14:24:11 +0200

    string methods

--- shopping_cart.py
+++ shopping_cart.py
@@ -3,7 +3,7 @@ print("**************************************\n")
 
 item = input("What would you like to buy? ")
 price = input(f"What is the price of the {item}? ")
-quantity = input(f"How many {item} are you buying? ")
+quantity = input(f"How many {item}s are you buying? ")
 
 print(f"\n{quantity} {item}(s) are added to the cart!")
 print(f"Subtotal: ${float(price) * int(quantity)}\n")
