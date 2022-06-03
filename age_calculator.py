636337d8dc1563a531df16af48b3dd93853f9a04
ealmiras <ealmirasozlu@gmail.com>
2022-06-03 13:09:45 +0200

    f string

--- age_calculator.py
+++ age_calculator.py
@@ -2,3 +2,4 @@ print("Oh hello! \nLet's calculate since how many days you're alive..")
 age = input("How old are you (in years)?")
 days = int(age) * 365
 print("You are " + str(days) + " days old!")
+print(f"You are {days} days old!")
